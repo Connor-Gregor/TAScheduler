cc08afc0496dc041a4555bbbe7365bc372fcafc4
Connor Gregorius <gregor66@uwm.edu>
2024-11-20 15:18:32 -0600

    Progress on implementing the login page

--- TAScheduler/urls.py
+++ TAScheduler/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from TASchedulerApp.views import Login, Dashboard
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Login.as_view()),
+    path('dashboard/', Dashboard.as_view())
 ]
--- TASchedulerApp/views.py
+++ TASchedulerApp/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 
 # Create your views here.
+class Login(View):
+    def get(self,request):
+        return render(request,"login.html")
+
+    def post(self, request):
+        name = request.POST['name']
+        password = request.POST['password']
+
+        # Check if username exists
+        #Change MyUser to name of class from models.py + include from .models import MyUser at the top
+        try:
+            user = MyUser.objects.get(name=name)
+            if user.password == password:  # Check if the password matches
+                request.session["name"] = user.name
+                return redirect("/dashboard/")  # Redirect to /dashboard/ on success
+            else:
+                # Incorrect password
+                return render(request, "login.html", {"message": "Incorrect password"})
+        except MyUser.DoesNotExist:
+            # Username does not exist
+            return render(request, "login.html", {"message": "User does not exist"})
+
+class Dashboard(View):
+    def get(self,request):
+        return render(request,"dashboard.html")
