b423d8cbdfc03f27ef4d71cb070b97a40a71d4a5
VictoriaRebecca <vectoria3508@gmail.com>
2024-11-26 16:51:31 -0600

    update

--- TASchedulerApp/views.py
+++ TASchedulerApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.contrib.auth.models import Group
 from .forms import RegistrationForm
@@ -6,6 +6,7 @@ from django.utils.decorators import method_decorator
 from .decorators import role_required
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 from .models import MyUser
 
@@ -81,3 +82,8 @@ class NotificationView(View):
     def get(self, request):
         # Logic for fetching and displaying notifications
         return render(request, 'common/notifications.html')
+
+@login_required
+def manage_users(request):
+    users = MyUser.objects.filter(role__in=['Instructor', 'TA'])
+    return render(request, 'admin/manage_users.html', {'users': users})
