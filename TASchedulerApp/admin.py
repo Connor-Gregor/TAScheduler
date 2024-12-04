977d6922ad6cd0efd7a1e0d3d992274377cbed0e
ahlawataryan <128097389+ahlawataryan@users.noreply.github.com>
2024-12-04 14:05:37 -0600

    Update admin.py

--- TASchedulerApp/admin.py
+++ TASchedulerApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import MyUser
 
-# Register your models here.
+@admin.register(MyUser)
+class MyUserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'role', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_active')
+    search_fields = ('name', 'email')
