353bca316b73aaec55b224a9a832104f10445602
ahlawataryan <128097389+ahlawataryan@users.noreply.github.com>
2024-12-04 14:04:56 -0600

    Update course_service.py

--- TASchedulerApp/service/course_service.py
+++ TASchedulerApp/service/course_service.py
@@ -1,4 +1,4 @@
-from TASchedulerApp.models import MyCourse
+from TASchedulerApp.models import MyCourse, MyUser
 
 class CourseService:
     @staticmethod
@@ -13,3 +13,25 @@ class CourseService:
         if 'new_time' in new_data:
             course.time = new_data['new_time']
         course.save()
+
+def assign_instructor_and_tas(course_id, instructor_id, ta_id):
+    try:
+        course = MyCourse.objects.get(id=course_id)
+
+        if instructor_id:
+            instructor = MyUser.objects.get(id=instructor_id, role='Instructor')
+            course.instructor = instructor
+
+        if ta_id:
+            ta = MyUser.objects.get(id=ta_id, role='TA')
+            course.tas.set([ta])
+
+        course.save()
+
+        return {"success": True, "message": "Assignments updated successfully."}
+    except MyCourse.DoesNotExist:
+        return {"success": False, "message": "Course not found."}
+    except MyUser.DoesNotExist:
+        return {"success": False, "message": "Instructor or TA not found."}
+    except Exception as e:
+        return {"success": False, "message": str(e)}
